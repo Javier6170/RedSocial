903269c1341e348decd632e8962bc1e791b2261d
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2021-11-03 19:12:23 -0500

    controller feature index

--- controlador/red_social_controller.py
+++ controlador/red_social_controller.py
@@ -5,6 +5,7 @@ import falcon
 
 class RedSocialController():
     def on_get(self, req, resp):
+        print("hola")
         resp.status = falcon.HTTP_200
         resp.content_type = 'text/html'
         with open("C:/Users/Javier/PycharmProjects/RedSocial/controlador/index.html", 'rb') as f:
