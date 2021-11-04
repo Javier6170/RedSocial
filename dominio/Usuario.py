78137e73f02b06c3fdb4cd728484ac21134d960e
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2021-11-03 19:35:36 -0500

    controller feature index

---
+++ dominio/Usuario.py
@@ -0,0 +1,7 @@
+class Usuario:
+    def __init__(self, nombre, apellido, usuario, contraseña, pais):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.usuario = usuario
+        self.contraseña = contraseña
+        self.pais = pais
