1ab81f91d0d172cd27a0bd97f6877a487108d530
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2021-11-06 12:20:55 -0500

    feature-aplication

--- dominio/Usuario.py
+++ dominio/Usuario.py
@@ -7,3 +7,45 @@ class Usuario(Cuenta):
         self.apellido = apellido
         self.usuario = usuario
         self.password = password
+
+    def _actualizar(self, id):
+        from infraestructura.persistencia_usuario import PersistenciaUsuario
+        persitencia_usuario = PersistenciaUsuario()
+        persitencia_usuario.actualizar(self, id)
+
+    def _guardar(self, usuario):
+        from infraestructura.persistencia_usuario import PersistenciaUsuario
+        persitencia_usuario = PersistenciaUsuario()
+        persitencia_usuario.guardar(self, usuario)
+
+    def guardar(self):
+        if self.id is None:
+            self._guardar(self.usuario)
+        else:
+            self._actualizar(self.id)
+
+    def update(self, dict_params):
+        self.nombre = dict_params.get('nombre', self.nombre)
+        self.apellido = dict_params.get('apellido', self.apellido)
+        self.usuario = dict_params.get('usuario', self.usuario)
+        self.password = dict_params.get('password', self.password)
+
+    def _actualizar(self, id):
+        persitencia_usuario = PersistenciaUsuario()
+        persitencia_usuario.actualizar(self, id)
+
+    def _guardar(self, usuario):
+        persitencia_usuario = PersistenciaUsuario()
+        persitencia_usuario.guardar(self, usuario)
+
+    def guardar(self):
+        if self.id is None:
+            self._guardar(self.usuario)
+        else:
+            self._actualizar(self.id)
+
+    def update(self, dict_params):
+        self.nombre = dict_params.get('nombre', self.nombre)
+        self.apellido = dict_params.get('apellido', self.apellido)
+        self.usuario = dict_params.get('usuario', self.usuario)
+        self.password = dict_params.get('password', self.password)
