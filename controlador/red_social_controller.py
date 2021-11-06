f3ec609e8e152716fb814c32d46d56a90e3b76a7
unknown <beltran.santiago.7813@eam.edu.co>
2021-11-06 12:45:19 -0500

    Metodos controlador

--- controlador/red_social_controller.py
+++ controlador/red_social_controller.py
@@ -1,6 +1,9 @@
 import codecs
 
 import falcon
+from infraestructura.persistencia_usuario import PersistenciaUsuario
+
+from dominio.Usuario import Usuario
 
 
 class RedSocialController():
@@ -9,4 +12,21 @@ class RedSocialController():
         resp.content_type = 'text/html'
         with open("C:/Users/usuario/Desktop/Trabajos Python/RedSocial/controlador/index.html", 'rb') as f:
             resp.body = f.read()
+
+
+    def on_post(self, req, resp):
+        cuenta= Usuario(**req.media)
+        cuenta.guardar()
+        resp.status = falcon.HTTP_CREATED
+
+    def on_put(self, req, resp, id):
+        cuenta_repositorio = PersistenciaUsuario()
+        usuario = cuenta_repositorio.cargar(id)
+        usuario.update(req.media)
+        usuario.id = id
+        usuario.guardar()
+        resp.body = usuario.__dict__
+
+    def on_delete(self):
+        pass
 
