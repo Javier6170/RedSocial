e486d7ca2fe3ae2b19df90e090f94157960a6299
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2021-11-06 12:30:07 -0500

    Modificanco usuario

--- dominio/Usuario.py
+++ dominio/Usuario.py
@@ -1,5 +1,6 @@
 from dominio.Cuenta import Cuenta
 
+
 class Usuario(Cuenta):
     def __init__(self, nombre, apellido, usuario, password):
         self.nombre = nombre
@@ -7,3 +8,45 @@ class Usuario(Cuenta):
         self.usuario = usuario
         self.password = password
         self.id = None
+
+    def _actualizar(self, id):
+        from infraestructura.persistencia_usuario import PersistenciaUsuario
+        persitencia_usuario = PersistenciaUsuario()
+        persitencia_usuario.actualizar(self, id)
+
+    def _guardar(self, usuario):
+        from infraestructura.persistencia_usuario import PersistenciaUsuario
+        persitencia_usuario = PersistenciaUsuario()
+        persitencia_usuario.guardar(self, usuario)
+
+    def guardar(self):
+        if self.id is None:
+            self._guardar(self.usuario)
+        else:
+            self._actualizar(self.id)
+
+    def update(self, dict_params):
+        self.nombre = dict_params.get('nombre', self.nombre)
+        self.apellido = dict_params.get('apellido', self.apellido)
+        self.usuario = dict_params.get('usuario', self.usuario)
+        self.password = dict_params.get('password', self.password)
+
+    def _actualizar(self, id):
+        persitencia_usuario = PersistenciaUsuario()
+        persitencia_usuario.actualizar(self, id)
+
+    def _guardar(self, usuario):
+        persitencia_usuario = PersistenciaUsuario()
+        persitencia_usuario.guardar(self, usuario)
+
+    def guardar(self):
+        if self.id is None:
+            self._guardar(self.usuario)
+        else:
+            self._actualizar(self.id)
+
+    def update(self, dict_params):
+        self.nombre = dict_params.get('nombre', self.nombre)
+        self.apellido = dict_params.get('apellido', self.apellido)
+        self.usuario = dict_params.get('usuario', self.usuario)
+        self.password = dict_params.get('password', self.password)
