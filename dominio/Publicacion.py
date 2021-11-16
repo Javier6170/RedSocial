fb1db0ccc238c8018eb54eb67774f6e11848be00
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2021-11-16 16:58:59 -0500

    Subiendo publicacion

--- dominio/Publicacion.py
+++ dominio/Publicacion.py
@@ -1,11 +1,16 @@
-class Publicacion(Usuario):
-    def __init__(self,textoArea):
-        self.textoArea = textoArea
-        self.id_p=None
+from dominio.Usuario import Usuario
 
+
+class Publicacion(Usuario):
+    def __init__(self, publicacion, id_user,nombre, apellido, usuario, password):
+        self.publicacion = publicacion
+        self.id_p = None
+        self.nombre = nombre
+        self.apellido = apellido
+        self.usuario = usuario
+        self.password = password
+        self.id = id_user
+        super().__init__(nombre, apellido, usuario, password)
 
     def __str__(self):
-        return f'{self.textoArea}---{self.id_p}'
-
-
-
+        return f'{self.publicacion}---{self.id_p}---'
